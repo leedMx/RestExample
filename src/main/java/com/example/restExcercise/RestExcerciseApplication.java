db688487d72c77e0bda2a2415c74d688c8c792e0
leedMx <esparza.mexico@gmail.com>
2020-12-15 08:12:29 -0600

    initial commit, h2 console enabled

---
+++ src/main/java/com/example/restExcercise/RestExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.restExcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restExcercise/RestExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restExcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
